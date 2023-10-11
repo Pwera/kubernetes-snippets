14bcd0c0a784f28a1405a8990a3df27868804963
pwera <piotr.wera.mail@gmail.com>
2023-10-11 21:27:49 +0200

    run redis through TcpRoute

--- 03-gateway-api-with-traefik/specific-files/script.sh
+++ 03-gateway-api-with-traefik/specific-files/script.sh
@@ -7,10 +7,13 @@ openssl genrsa -out certs/tls.key 2048
 openssl req -new -key certs/tls.key -out certs/tls.csr -subj "/CN=whoami.localhost"
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:whoami.localhost") -in certs/tls.csr -signkey certs/tls.key -out certs/tls.crt
 
-openssl genrsa -out certs/tls.key 2048
-openssl req -new -key certs/tls.key -out cesrts/tls.csr -subj "/CN=katacoda.com"
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:webhook-server.production.svc,DNS:*.2887302149-30001-host12nc.environments.katacoda.com") -in certs/tls.csr -signkey certs/tls.key -out certs/tls.crt
-
 kubectl create secret tls mysecret     --cert "certs/tls.crt"     --key "certs/tls.key"
 
 kubectl taint node master-node node-role.kubernetes.io/control-plane-
+
+wget https://github.com/holys/redis-cli/releases/download/v0.0.2/redis-cli-v0.0.2-linux-amd64
+mv redis-cli-v0.0.2-linux-amd64 redis-cli
+mv redis-cli /usr/local/bin
+
+redis-cli -h localhost -p 30000 SET key value
+
